3ae026d677ddab0e7be5fcc9e137cce1bc3d5656
Keshu Kumar <Keshukumar1909@gmail.com>
2025-09-03 23:27:06 +0530

    adding the college model to register them in the erp

---
+++ model/CollegeModel.js
@@ -0,0 +1,38 @@
+const mongoose = require('mongoose');
+
+const collegeSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: [true, "Please Provide College name"],
+        trim: true
+    },
+
+    collegeId: {
+        type: String,
+        required: [true, "Please Provide College Id"],
+        unique: true
+    },
+
+    courses: [
+        {
+            courseName: { type: String, required: true },
+            timePeriod: { type: String, required: true }
+        }
+    ],
+
+    uniqueErpId: {
+        type: String,
+        unique: true   
+    },
+
+    websiteUrl: {
+        type: String
+    },
+
+    contactNumber: {
+        type: String
+    }
+}, { timestamps: true });
+
+const collegeModel = mongoose.model('College', collegeSchema);
+module.exports = collegeModel;
